0ce60c82e6bb67ca57ef878d6ea80c04b239a228
Cristi Constantin <cristi.constantin@live.com>
2016-09-03 17:57:17 +0300

    :bread: Updated tests to support ZIP files;

--- test/seven_helpers_test.exs
+++ test/seven_helpers_test.exs
@@ -2,7 +2,7 @@ defmodule FileSmasherSevenHelpersTest do
   use ExUnit.Case
   alias FileSmasher.SevenZip
 
-  test "parse info 7z file + folder" do
+  test "parse info 7z files in folder" do
     o = "Listing archive: test/documents.7z\n\n--\nPath = test/documents.7z\nType = 7z\nPhysical Size = 7550944\nHeaders Size = 307\nMethod = LZMA2:23\nSolid = +\nBlocks = 1\n\n   Date      Time    Attr         Size   Compressed  Name\n------------------- ----- ------------ ------------  ------------------------\n2016-08-31 16:39:40 D....            0            0  test/documents\n2016-08-31 16:36:29 ....A      6010879      7550637  test/documents/Comfort_Fit - Sorry.mp3\n2016-08-31 16:25:23 ....A       518076               test/documents/Elixir-Lang.pdf\n2016-08-31 16:39:13 ....A      1129054               test/documents/photo-1443641998979-d59cfcf800c4.jpeg\n------------------- ----- ------------ ------------  ------------------------\n2016-08-31 16:39:40            7658009      7550637  3 files, 1 folders\n"
     m = SevenZip.parse_list_output o
     assert m["type"] == "7z"
@@ -16,6 +16,7 @@ defmodule FileSmasherSevenHelpersTest do
     assert m["type"] == "7z"
     assert m["solid"] == false
     assert m["files"] == 1
+    assert m["arch_bytes"] == 511010
   end
 
   test "parse info 7z 2 files" do
@@ -24,6 +25,15 @@ defmodule FileSmasherSevenHelpersTest do
     assert m["type"] == "7z"
     assert m["solid"] == false
     assert m["files"] == 2
+    assert m["arch_bytes"] == 1625933
+  end
+
+  test "parse info zip files in folder" do
+    o = "Listing archive: test/documents.zip\n\n--\nPath = test/documents.zip\nType = zip\nPhysical Size = 7520721\n\n   Date      Time    Attr         Size   Compressed  Name\n------------------- ----- ------------ ------------  ------------------------\n2016-09-03 17:30:16 D....            0            0  documents\n2016-08-31 16:36:29 .....      6010879      5899804  documents/Comfort_Fit - Sorry.mp3\n2016-08-31 16:25:23 .....       518076       506271  documents/Elixir-Lang.pdf\n2016-08-31 16:39:13 .....      1129054      1113998  documents/photo-1.jpg\n------------------- ----- ------------ ------------  ------------------------\n2016-09-03 17:30:16            7658009      7520073  3 files, 1 folders\n"
+    m = SevenZip.parse_list_output o
+    assert m["type"] == "zip"
+    assert m["files"] == 3
+    assert m["arch_bytes"] == 7520721
   end
 
 end
--- test/seven_test.exs
+++ test/seven_test.exs
@@ -2,10 +2,25 @@ defmodule FileSmasherSevenTest do
   use ExUnit.Case, async: false
   alias FileSmasher.SevenZip
 
+  setup_all do
+    arch_7z = System.cwd <> "/test/documents.7z"
+    arch_zip = System.cwd <> "/test/documents.zip"
+    if File.regular?(arch_7z) do
+      File.rm(arch_7z)
+      IO.puts "Deleted old 7z achive.."
+    end
+    if File.regular?(arch_zip) do
+      File.rm(arch_zip)
+      IO.puts "Deleted old zip achive.."
+    end
+    :ok
+  end
+
   test "compress folder with 7z min" do
     path = System.cwd <> "/test/documents"
     o1 = SevenZip.compress(path, {:'7z', :min}) |> IO.inspect
     o2 = SevenZip.info(System.cwd <> "/test/documents.7z") |> IO.inspect
+    assert o2["files"] == 3
     assert o2["type"] == "7z"
     assert o2["solid"] == false
     assert o1["o_bytes"] == o2["o_bytes"]
@@ -16,8 +31,29 @@ defmodule FileSmasherSevenTest do
     path = System.cwd <> "/test/documents"
     o1 = SevenZip.compress(path, {:'7z', :ultra}) |> IO.inspect
     o2 = SevenZip.info(System.cwd <> "/test/documents.7z") |> IO.inspect
+    assert o2["files"] == 3
     assert o2["type"] == "7z"
     assert o2["solid"] == true
+    assert o1["o_bytes"] == o2["o_bytes"]
+    assert o1["arch_bytes"] == o2["arch_bytes"]
+  end
+
+  test "compress folder with zip min" do
+    path = System.cwd <> "/test/documents"
+    o1 = SevenZip.compress(path, {:zip, :min}) |> IO.inspect
+    o2 = SevenZip.info(System.cwd <> "/test/documents.zip") |> IO.inspect
+    assert o2["type"] == "zip"
+    assert o2["files"] == 3
+    assert o1["o_bytes"] == o2["o_bytes"]
+    assert o1["arch_bytes"] == o2["arch_bytes"]
+  end
+
+  test "compress folder with zip ultra" do
+    path = System.cwd <> "/test/documents"
+    o1 = SevenZip.compress(path, {:zip, :ultra}) |> IO.inspect
+    o2 = SevenZip.info(System.cwd <> "/test/documents.zip") |> IO.inspect
+    assert o2["type"] == "zip"
+    assert o2["files"] == 3
     assert o1["o_bytes"] == o2["o_bytes"]
     assert o1["arch_bytes"] == o2["arch_bytes"]
   end
