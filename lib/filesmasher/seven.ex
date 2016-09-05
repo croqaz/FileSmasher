e348467e5f2bba5735b1501d7e2a764af96ef4f4
Cristi Constantin <cristi.constantin@live.com>
2016-09-05 22:28:53 +0300

    :muscle: Completed testing; Fixed a bug on parsing extract output;

--- lib/filesmasher/seven.ex
+++ lib/filesmasher/seven.ex
@@ -38,10 +38,18 @@ defmodule FileSmasher.SevenZip do
   @doc """
   Compress a file, or folder.
 
-  Options:
+  ##Methods:
 
-    - {:'7z'} - normal 7Z, the default option
-    - {:zip} - normal ZIP compression
+  * {:'7z'} - normal 7Z, the default option
+  * {:'7z', :min} - 7Z minimal compression (strength 1/9)
+  * {:'7z', :fast} - 7Z fast compression (strength 3/9)
+  * {:'7z', :max} - 7Z strong compression (strength 7/9, using LZMA2)
+  * {:'7z', :ultra} - 7Z maximum possible compression (strength 9/9, using LZMA2)
+  * {:zip} - normal ZIP compression
+  * {:zip, :min} - ZIP minimal compression (strength 1/9)
+  * {:zip, :fast} - ZIP fast compression (strength 3/9)
+  * {:zip, :max} - ZIP strong compression (strength 7/9, using Deflate64)
+  * {:zip, :ultra} - ZIP maximum possible compression (strength 9/9, using Deflate64)
   """
   @spec compress(String.t, String.t, tuple, list) :: map
   def compress(arch, path, method \\ {:'7z'}, args \\ []) do
@@ -56,15 +64,25 @@ defmodule FileSmasher.SevenZip do
   @doc false
   def parse_extract_output(output) do
     match = Regex.named_captures(~r/
-      ^Files:\s+(?<files>\d+)$.+
+      ^Type\s=\s(?<type>\S+).+
       ^Size:\s+(?<o_bytes>\d+)$.+
       ^Compressed:\s+(?<arch_bytes>\d+)$
       /xms, output)
+    match = case String.match?(output, ~r/Files: \d/) do
+      true -> Map.put(match, "files", Regex.run(~r/^Files: (\d+)$/m, output) |> Enum.at(1))
+      false -> Map.put(match, "files", "1")
+    end
+    match = case String.match?(output, ~r/Method = /) do
+      true -> Map.put(match, "method", Regex.run(~r/^Method = (\S+)$/m, output) |> Enum.at(1))
+      false -> match
+    end
     fix_common_match(match)
   end
 
   @doc """
   Extract archive into a path.
+
+  ##Overwrite existing files: true, or false.
   """
   @spec extract(String.t, String.t, boolean) :: map
   def extract(arch, path, overwrite \\ false) do
--- test/seven_helpers_test.exs
+++ test/seven_helpers_test.exs
@@ -16,6 +16,7 @@ defmodule FileSmasherSevenHelpersTest do
     assert m["type"] == "7z"
     assert m["solid"] == false
     assert m["files"] == 1
+    assert m["o_bytes"] == 518076
     assert m["arch_bytes"] == 511010
   end
 
@@ -25,6 +26,7 @@ defmodule FileSmasherSevenHelpersTest do
     assert m["type"] == "7z"
     assert m["solid"] == false
     assert m["files"] == 2
+    assert m["o_bytes"] == 1647130
     assert m["arch_bytes"] == 1625933
   end
 
@@ -33,7 +35,101 @@ defmodule FileSmasherSevenHelpersTest do
     m = SevenZip.parse_list_output o
     assert m["type"] == "zip"
     assert m["files"] == 3
+    assert m["o_bytes"] == 7658009
     assert m["arch_bytes"] == 7520721
+  end
+
+  # --- ADD ---
+
+  test "parse adding a folder with 7z" do
+    o = "Scanning the drive:\n2 folders, 3 files, 7658009 bytes (7479 KiB)\n\nCreating archive: test/documents.7z\n\nItems to compress: 5\n\n\nFiles read from disk: 3\nArchive size: 7550926 bytes (7374 KiB)\nEverything is Ok\n"
+    m = SevenZip.parse_add_output o
+    assert m["files"] == 3
+    assert m["o_bytes"] == 7658009
+    assert m["arch_bytes"] == 7550926
+  end
+
+  test "parse adding a file with 7z" do
+    o = "Scanning the drive:\n1 file, 518076 bytes (506 KiB)\n\nCreating archive: test/documents.7z\n\nItems to compress: 1\n\n\nFiles read from disk: 1\nArchive size: 511010 bytes (500 KiB)\nEverything is Ok\n"
+    m = SevenZip.parse_add_output o
+    assert m["files"] == 1
+    assert m["o_bytes"] == 518076
+    assert m["arch_bytes"] == 511010
+  end
+
+  test "parse adding a folder with ZIP" do
+    o = "Scanning the drive:\n2 folders, 3 files, 7658009 bytes (7479 KiB)\n\nCreating archive: test/documents.zip\n\nItems to compress: 5\n\n\nFiles read from disk: 3\nArchive size: 7546665 bytes (7370 KiB)\nEverything is Ok\n"
+    m = SevenZip.parse_add_output o
+    assert m["files"] == 3
+    assert m["o_bytes"] == 7658009
+    assert m["arch_bytes"] == 7546665
+  end
+
+  test "parse adding a file with ZIP" do
+    o = "Scanning the drive:\n1 file, 518076 bytes (506 KiB)\n\nCreating archive: test/documents.zip\n\nItems to compress: 1\n\n\nFiles read from disk: 1\nArchive size: 508193 bytes (497 KiB)\nEverything is Ok\n"
+    m = SevenZip.parse_add_output o
+    assert m["files"] == 1
+    assert m["o_bytes"] == 518076
+    assert m["arch_bytes"] == 508193
+  end
+
+  # --- EXTRACT ---
+
+  test "parse extracting a folder with ZIP" do
+    o = "Scanning the drive for archives:\n1 file, 7546665 bytes (7370 KiB)\n\nExtracting archive: test/documents.zip\n--\nPath = test/documents.zip\nType = zip\nPhysical Size = 7546665\n\nEverything is Ok\n\nFolders: 2\nFiles: 3\nSize:       7658009\nCompressed: 7546665\n"
+    m = SevenZip.parse_extract_output o
+    assert m["type"] == "zip"
+    assert m["files"] == 3
+    assert m["o_bytes"] == 7658009
+    assert m["arch_bytes"] == 7546665
+  end
+
+  test "parse extracting 2 files with ZIP" do
+    o = "Scanning the drive for archives:\n1 file, 1623632 bytes (1586 KiB)\n\nExtracting archive: test/documents.zip\n--\nPath = test/documents.zip\nType = zip\nPhysical Size = 1623632\n\nEverything is Ok\n\nFiles: 2\nSize:       1647130\nCompressed: 1623632\n"
+    m = SevenZip.parse_extract_output o
+    assert m["type"] == "zip"
+    assert m["files"] == 2
+    assert m["o_bytes"] == 1647130
+    assert m["arch_bytes"] == 1623632
+  end
+
+  test "parse extracting a file with ZIP" do
+    o = "Scanning the drive for archives:\n1 file, 508193 bytes (497 KiB)\n\nExtracting archive: test/documents.zip\n--\nPath = test/documents.zip\nType = zip\nPhysical Size = 508193\n\nEverything is Ok\n\nSize:       518076\nCompressed: 508193\n"
+    m = SevenZip.parse_extract_output o
+    assert m["type"] == "zip"
+    assert m["files"] == 1
+    assert m["o_bytes"] == 518076
+    assert m["arch_bytes"] == 508193
+  end
+
+  test "parse extracting a folder with 7z" do
+    o = "Scanning the drive for archives:\n1 file, 7550927 bytes (7374 KiB)\n\nExtracting archive: test/documents.7z\n--\nPath = test/documents.7z\nType = 7z\nPhysical Size = 7550927\nHeaders Size = 290\nMethod = LZMA2:23\nSolid = +\nBlocks = 1\n\nEverything is Ok\n\nFolders: 2\nFiles: 3\nSize:       7658009\nCompressed: 7550927\n"
+    m = SevenZip.parse_extract_output o
+    assert m["type"] == "7z"
+    assert m["files"] == 3
+    assert m["o_bytes"] == 7658009
+    assert m["arch_bytes"] == 7550927
+    assert String.split(m["method"], ":") |> hd == "LZMA2"
+  end
+
+  test "parse extracting 2 files with 7z" do
+    o = "Scanning the drive for archives:\n1 file, 1625901 bytes (1588 KiB)\n\nExtracting archive: test/documents.7z\n--\nPath = test/documents.7z\nType = 7z\nPhysical Size = 1625901\nHeaders Size = 223\nMethod = LZMA2:1536k\nSolid = -\nBlocks = 2\n\nEverything is Ok\n\nFiles: 2\nSize:       1647130\nCompressed: 1625901\n"
+    m = SevenZip.parse_extract_output o
+    assert m["type"] == "7z"
+    assert m["files"] == 2
+    assert m["o_bytes"] == 1647130
+    assert m["arch_bytes"] == 1625901
+    assert String.split(m["method"], ":") |> hd == "LZMA2"
+  end
+
+  test "parse extracting a file with 7z" do
+    o = "Scanning the drive for archives:\n1 file, 511010 bytes (500 KiB)\n\nExtracting archive: test/documents.7z\n--\nPath = test/documents.7z\nType = 7z\nPhysical Size = 511010\nHeaders Size = 170\nMethod = LZMA2:19\nSolid = -\nBlocks = 1\n\nEverything is Ok\n\nSize:       518076\nCompressed: 511010\n"
+    m = SevenZip.parse_extract_output o
+    assert m["type"] == "7z"
+    assert m["files"] == 1
+    assert m["o_bytes"] == 518076
+    assert m["arch_bytes"] == 511010
+    assert String.split(m["method"], ":") |> hd == "LZMA2"
   end
 
 end
